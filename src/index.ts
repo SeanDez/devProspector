5ab1ed9afcbb26cb764b290a0cdbfc8ce8b65a55
Sean Dez <sd@seandezoysa.com>
2020-09-30 03:40:28 +0700

    draft message class

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,17 @@ import envTyped from './shared/envVariablesTyped';
 
 const { HUBSPOT_API_KEY } = envTyped;
 
+/*
+  Get all contacts that have not been contacted and have emails
+
+  For each selection
+  Send each one an email
+  Kick out contacts where all contact attempts failed
+  if email was a success
+    create an email event in hubspot
+    change status to contacted (or similar)
+*/
+
 const hubspotClient = new hubspot.Client({ apiKey: HUBSPOT_API_KEY });
 
 hubspotClient.crm.timeline.eventsApi.create
