fffa817e8b26cd9034c0bd20e4021e333cddc01b
Sean Dez <sd@seandezoysa.com>
2020-10-04 04:32:06 +0700

    add body parser to fix missing post body

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import Express from 'express';
+import bodyParser from 'body-parser';
 import cors from 'cors';
+import Express from 'express';
 import moment from 'moment';
 
 import customPropertiesRouter from './customProperties/router';
@@ -10,12 +11,19 @@ const { SERVER_PORT } = envTyped;
 const server = Express();
 
 server
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: false }))
   .use(cors())
   .use(customPropertiesRouter);
+
+// to be deleted after dev phase
+server.get('/', (req: Express.Request, res: Express.Response) => {
+  res.json({ message: 'this endpoint is just for basic server testing' });
+});
 
 server.listen(SERVER_PORT, () => {
   console.log(`Express server running on port ${SERVER_PORT}`);
 
-  const now: string = moment().format('h:mm:ss a [on] MMM Do[,] YYYY');
+  const now: string = moment().format('h:mm:ssa [on] MMM Do[,] YYYY');
   console.log(`Last restarted: ${now}`);
 });
